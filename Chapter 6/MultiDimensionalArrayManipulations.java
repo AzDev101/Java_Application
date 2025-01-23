0130f91f051db8bc5efc4da3a93765ff0086b92a
AzDev101 <razaklekan277@gmail.com>
2025-01-23 11:34:55 +0100

    Chapter 6

---
+++ Chapter 6/MultiDimensionalArrayManipulations.java
@@ -0,0 +1,14 @@
+public class MultiDimensionalArrayManipulations{
+	public static void main (String [] args){
+		int [][] numbers = {{3,12,2,6,5}, {7,12,5,2,8}};
+		int sum = 0;
+		
+		for (int row = 0; row < 2; row++){
+			for(int col = 0; col < 5; col++){
+				
+				sum += numbers[row][col];
+			}
+		}
+		System.out.printf("The total number is %d", sum);
+	}
+}
---
+++ Chapter 6/UsingArraysClass.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class UsingArraysClass{
+	public static void main(String [] args){
+		int [] numbers = {6,19,2,29,1,30,8,3,12,4};
+		
+		//arrays.sort(numbers)...sort method
+		Arrays.sort(numbers); //Arrays.sort(numbers,4,9); for an index sorting
+		
+		for(int element : numbers){
+			System.out.printf("%d%n", element);
+		}
+		System.out.println("===================================");
+		
+		//binary search method
+		int index = Arrays.binarySearch(numbers,30);
+		System.out.printf("The index numbers of 30 is %d%n", index);
+		System.out.println("===================================");
+		
+		//Equals static method for coparing two arrays
+		int[] arr1 = {1,2,3};
+		int[] arr2 = {1,2,3};
+		boolean isEqual = Arrays.equals(arr1, arr2);
+		
+		System.out.printf("%b%n", isEqual);	
+		
+		System.out.println("===================================");
+		
+		//Fill method...used to fill an array
+		int [] myArrays = new int [5];
+		
+		Arrays.fill(myArrays,20);
+		
+		for(int component : myArrays){
+			System.out.printf("%d%n", component);
+		}	
+		System.out.println("===================================");
+		
+		//copy an array
+		int[] original = {1,2,3};
+		int[] copy = Arrays.copyOf(original, 5);
+		
+		for(int value : copy){
+			System.out.printf("%d%n", value);
+		}	
+		System.out.println("===================================");
+		
+		//Converting Arrays(sd[] = toString method and the md[] = deepToString method)
+		
+		String copyArray = Arrays.toString(copy);
+		
+		System.out.printf("%s%n", copyArray);	
+		System.out.println("===================================");
+	}
+}
