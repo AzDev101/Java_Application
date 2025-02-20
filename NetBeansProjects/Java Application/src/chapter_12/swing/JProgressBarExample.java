96a1ec428c32c0f2fd26ddd10002d43f79cd20c9
AzDev101 <razaklekan277@gmail.com>
2025-02-20 23:06:47 +0100

    Netbeans Files

--- NetBeansProjects/Java Application/src/chapter_12/swing/JProgressBarExample.java
+++ NetBeansProjects/Java Application/src/chapter_12/swing/JProgressBarExample.java
@@ -1,19 +1,23 @@
 
 package chapter_12.swing;
-import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JProgressBar;
+import javax.swing.JPanel;
 
 public class JProgressBarExample {
     public static void main(String[] args) {
        JFrame frame = new JFrame("Progress Bar Demo");
        JProgressBar progressBar = new JProgressBar(0,100);
+       JPanel panel = new JPanel();
        
-       progressBar.setValue(40);
+       progressBar.setValue(50);
        progressBar.setStringPainted(true);
        
-       frame.add(progressBar);
-       frame.setSize(500,50);
+       panel.add(progressBar);
+       
+      
+       frame.add(panel);
+       frame.setSize(500,500);
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setVisible(true);
        
